01237f6eb0ab759d35900d8f19649d3052edf8bb
Inna <21icelady97@gmail.com>
2019-03-17 16:52:04 +0300

    date

--- js/slider.js
+++ js/slider.js
@@ -246,20 +246,60 @@ $(document).ready(function() {
 })
 
 $(window).resize(function() {
-    var date = new Date();
-    var currentMonth = date.getMonth() + 1,
-    currentYear = date.getFullYear(),
-    currentDate = currentMonth+''+currentYear;
+  var date = new Date();
+  var currentMonth = date.getMonth() + 1,
+  currentYear = date.getFullYear(),
+  currentDate = currentMonth+''+currentYear;
 
-    var calendarItems = $('.calendar-item');
-    var currentItem = calendarItems.index($('.calendar-item[id='+currentDate+']'));
+  var calendarItems = $('.calendar-item');
+  var currentItem = calendarItems.index($('.calendar-item[id='+currentDate+']'));  
 
-    var slideNow = 1,
-    slideCount = $('.listslider').children().length,
-    translateWidth = 0,
-    slideInterval = 2000;
+  if ($(window).width() <= 551) {
+    currentDate = currentYear + ', ' + currentMonth + ', ' + date.getDate();
+    currentItem = calendarItems.index($('.calendar-item[data-date='+ "'" +currentDate+ "'" +']'));
+  }
 
-    if (currentItem != -1) {
+  var slideNow = 1,
+  slideCount = $('.listslider').children().length,
+  translateWidth = 0;
+
+  if (currentItem != -1) {
+
+    translateWidth = -$('#slider').width()*(currentItem);
+    $('.listslider').css({
+      'transform':'translate('+translateWidth + 'px, 0)',
+      '-webkit-transform':'translate('+translateWidth + 'px, 0)',
+      '-ms-transform':'translate('+translateWidth + 'px, 0)',
+    });
+    slideNow = currentItem + 1;
+    translateWidth = 0;
+
+  }
+  else {
+
+    if ($(window).width() <= 551) {
+      var currentItems = [];
+      for (var i = 0; i < slideCount; i++) {
+        currentItems.push($(calendarItems[i]).data('date'));
+      }
+
+      var numCurrentItems = [];
+      for (var i = 0; i < slideCount; i++) {
+        numCurrentItems.push(new Date(currentItems[i]));
+      }
+
+      var numCurrentItem;
+      for (var i = 0; i < slideCount; i++) {
+        if (date < numCurrentItems[i]) {
+          numCurrentItem = numCurrentItems[i];
+          break;
+        }
+      }
+
+      currentMonth = numCurrentItem.getMonth() + 1;
+      currentDate = numCurrentItem.getFullYear() + ', ' + currentMonth + ', ' + numCurrentItem.getDate();
+     
+      currentItem = calendarItems.index($('.calendar-item[data-date='+ "'" +currentDate+ "'" +']'));  
       translateWidth = -$('#slider').width()*(currentItem);
       $('.listslider').css({
         'transform':'translate('+translateWidth + 'px, 0)',
@@ -268,7 +308,8 @@ $(window).resize(function() {
       });
       slideNow = currentItem + 1;
       translateWidth = 0;
-    }
+    } 
+
     else {
       currentItem = $(calendarItems[calendarItems.length-1]).index();
       translateWidth = -$('#slider').width()*(currentItem);
@@ -280,38 +321,5 @@ $(window).resize(function() {
       slideNow = currentItem + 1;
       translateWidth = 0;
     }
-
-    function nextSlide() {
-      if (slideNow == slideCount || slideNow <= 0 || slideNow > slideCount) {
-        $('.listslider').css('transform', 'translate(0,0)');
-        slideNow = 1;
-      } else {
-        translateWidth = -$('#slider').width()*(slideNow);
-        $('.listslider').css({
-          'transform':'translate('+translateWidth + 'px, 0)',
-          '-webkit-transform':'translate('+translateWidth + 'px, 0)',
-          '-ms-transform':'translate('+translateWidth + 'px, 0)',
-        });
-        slideNow++;
-      }
-    }
-    function prevSlide() {
-        if (slideNow == 1 || slideNow <= 0 || slideNow > slideCount) {
-          translateWidth = -$('#slider').width() * (slideCount - 1);
-          $('.listslider').css({
-              'transform': 'translate(' + translateWidth + 'px, 0)',
-              '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
-              '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
-          });
-          slideNow = slideCount;
-        } else {
-          translateWidth = -$('#slider').width() * (slideNow - 2);
-          $('.listslider').css({
-              'transform': 'translate(' + translateWidth + 'px, 0)',
-              '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
-              '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
-          });
-          slideNow--;
-      }
-    }
+  }
 });
